0a27f1ca6663a547caa313b5572b8655f9fd7e89
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-03-05 15:56:20 +0200

    Completed readImages function implementation to read in all raw data files into a slices vector

--- VolImage.cpp
+++ VolImage.cpp
@@ -35,41 +35,45 @@ namespace SCRSHA001{
 
     bool VolImage::readImages(std::string baseName) {
 
-        string fileDirectory = "brain_mri_raws/";
-        ifstream headerFile(fileDirectory + baseName + ".data");
+        string fileDirectory = "brain_mri_raws/"; //Directory where raw files are kept
+        ifstream headerFile(fileDirectory + baseName + ".data"); //Access header file
         string headerInfo;
-        getline(headerFile,headerInfo);
-        stringstream streamOfHeaderInfo (headerInfo);
+        getline(headerFile,headerInfo); //Get first line from header
+        stringstream streamOfHeaderInfo (headerInfo); //Take in header info as string stream to manipulate
 
+        //Store info from header
         int numOfFiles;
         streamOfHeaderInfo >> width;
         streamOfHeaderInfo >> height;
         streamOfHeaderInfo >> numOfFiles;
 
-        cout <<"Gets hernkje " << (width) <<" " << height << " " << numOfFiles<<endl;
-
+        //Insert binary data from raw files into slices array
         for (int i = 0; i < numOfFiles; i++) {
             cout<< fileDirectory + baseName   + to_string(i) +  ".raw" <<endl;
+            //Access raw file as binary
             ifstream rawFile(fileDirectory + baseName   + to_string(i) +  ".raw",ios::binary);
-            if (rawFile.is_open()){
+
+            if (rawFile.is_open()){ //Check if open
+                //Create a pointer char array representing a whole Image (slice) - add pointer to this array into slices vector
                 slices.push_back( (unsigned char **) new char**[height]);
                 for (int j = 0; j < height; ++j) {
+                    //Create a char array for each row in the image (height)
                     slices[i][j] = (unsigned char *) new char*[width];
                     for (int k = 0; k < width; ++k) {
                         //cout <<rawFile.get() <<" ";
-
+                        //Add binary data to each element in the row
                         slices[i][j][k] = (unsigned char) char(rawFile.get());
                     }
-                    //cout << endl;
-                   // cout << "loop" <<endl;
                 }
             }
-            cout << "Done with File"<<endl;
+            else{ //If ever can't open a raw data file - return false
+                return false;
+            }
+            //cout << "Done with File"<<endl;
         }
-        cout<< slices[0][1][2];
+        cout<< to_string(slices[0][1][2]) << endl;
         cout <<"end of loop"<<endl;
-        //todo Read in as in binary format
-        return false;
+        return true; //If could open all files -return true
     }
 
     void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
