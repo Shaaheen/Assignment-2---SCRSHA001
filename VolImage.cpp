19e2f8d493c9467d2d5c2a360007104570fdd5a6
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-03-05 15:03:28 +0200

    Can take in info from the raw header file to check what structure of raw files are like

--- VolImage.cpp
+++ VolImage.cpp
@@ -34,7 +34,44 @@ namespace SCRSHA001{
         height= 0;
     }
 
+    bool VolImage::readImages(std::string baseName) {
 
+        string fileDirectory = "brain_mri_raws/";
+        ifstream headerFile(fileDirectory + baseName + ".data");
+        string headerInfo;
+        getline(headerFile,headerInfo);
+        stringstream streamOfHeaderInfo (headerInfo);
+
+        int numOfFiles;
+        streamOfHeaderInfo >> width;
+        streamOfHeaderInfo >> height;
+        streamOfHeaderInfo >> numOfFiles;
+
+        cout <<"Gets hernkje " << (width) <<" " << height << " " << numOfFiles<<endl;
+
+        for (int i = 0; i < numOfFiles; i++) {
+            cout<< fileDirectory + baseName   + to_string(i) +  ".raw" <<endl;
+            ifstream rawFile(fileDirectory + baseName   + to_string(i) +  ".raw",ios::binary);
+            if (rawFile.is_open()){
+                slices.push_back( (unsigned char **) new char[height]);
+                for (int j = 0; j < height; ++j) {
+                    slices[i][j] = (unsigned char *) new char[width];
+                    for (int k = 0; k < width; ++k) {
+                        //cout <<rawFile.get() <<" ";
+
+                        slices[i][j][k] = (unsigned char) rawFile.get();
+                    }
+                    //cout << endl;
+                   // cout << "loop" <<endl;
+                }
+            }
+            cout << "Done with File"<<endl;
+        }
+        cout<< slices[0][1][2];
+        cout <<"end of loop"<<endl;
+        //todo Read in as in binary format
+        return false;
+    }
 
     void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
 
